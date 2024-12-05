59cd78dd225a388e1f5fe1de1cd297d5c98680a9
Felipe rei dos roblox ratos e fofos <felipe.ado@estudante.iftm.edu.br>
2024-12-05 08:41:33 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,241 @@
+const data = [
+   
+    {
+        Image: "fotos/bicicleta-aro-26-portal-wheeling-perifa-2-0-14-5-lilas-com-roxo-pneu-vee-rubber-e-freio-hidraulico-shimano-66ad38bc70bb8 (1).jpg",
+        title:"BICICLETA ARO 26  2.0 - 14,5 - LÍLAS COM ROXO, PNEU VEE RUBBER E FREIO HIDRAÚLICO SHIMANO",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/bicicleta-aro-29-absolute-nero-5-15-cinza-fosco-e-vermelho-com-freio-a-disco-671be8e86ca4a.jpg",
+        title:"BICICLETA ARO 29 ABSOLUTE NERO 5 - 15 - CINZA FOSCO E VERMELHO COM FREIO A DISCO MECÂNICO",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/bicicleta-gios-aro-26-4trix-preta-e-roxa-com-freio-a-disco-hidraulico-shimano-66d73161767fc.jpg",
+        title:"BICICLETA GIOS 4TRIX ARO 26 - PRETA E ROXA COM FREIO A DISCO HIDRÁULICO SHIMANO",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/bicicleta-aro-26-portal-wheeling-perifa-2-0-14-5-preta-e-verde-com-verde-neon-66997159e2859 (1).jpg",
+        title:"BICICLETA ARO 26 PORTAL WHEELING PERIFA 1.0 - 14,5 - PRETA E VERDE COM VERDE NEON",
+        description: "comprar",
+    },
+   
+    {
+        Image: "fotos/bicicleta-aro-26-portal-wheeling-perifa-2-0-14-5-lilas-com-roxo-pneu-vee-rubber-e-freio-hidraulico-shimano-66ad38bc70bb8 (1).jpg",
+        title:"BICICLETA ARO 26 PORTAL WHEELING PERIFA 2.0 - 14,5 - LÍLAS COM ROXO, PNEU VEE RUBBER E FREIO HIDRAÚLICO SHIMANO",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/bicicleta-gios-4trix-aro-26-vermelha-e-preta-com-freio-a-disco-hidraulico-shimano-66e9990338591 (1).jpg",
+        title:"BICICLETA GIOS 4TRIX ARO 26 - VERMELHA E PRETA COM FREIO A DISCO HIDRÁULICO SHIMANO",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/bicicleta-gios-aro-26-4trix-rosa-e-branca-66c73306e3fef.jpg",
+        title:"BICICLETA GIOS 4TRIX ARO 26 - ROSA E BRANCACod. do Produto: 5526 ",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/bicicleta-gios-4trix-aro-26-preto-e-azul-com-freio-a-disco-hidraulico-shimano-66db5c7e6b90b.jpg",
+        title:"BICICLETA GIOS 4TRIX ARO 26 - PRETO E AZUL COM FREIO A DISCO HIDRÁULICO SHIMANO",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/bicicleta-gios-aro-26-4trix-branca-e-preta-com-freio-a-disco-hidraulico-shimano-66d84bc5c5462 (1).jpg",
+        title:"BICICLETA GIOS 4TRIX ARO 26 - BRANCA E PRETA COM FREIO A DISCO HIDRÁULICO SHIMANO",
+        description: "comprar",
+    },
+
+
+
+
+    {
+        Image: "fotos/bicicleta-gios-4trix-aro-26-amarelo-e-preto-com-freio-a-disco-hidraulico-shimano-66ed6b162d327.jpg",
+        title:"BICICLETA GIOS 4TRIX ARO 26 - AMARELO E PRETO COM FREIO A DISCO HIDRÁULICO SHIMANO",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/bicicleta-gios-4trix-aro-26-preto-com-freio-a-disco-hidraulico-shimano-66ed50a3b6580.jpg",
+        title:"BICICLETA GIOS 4TRIX ARO 26 - PRETO COM FREIO A DISCO HIDRÁULICO SHIMANO",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/bicicleta-gios-4trix-aro-26-azul-claro-e-preto-com-freio-a-disco-hidraulico-shimano-66f2968b5f20b.jpg",
+        title:"BICICLETA GIOS 4TRIX ARO 26 - AZUL CLARO E PRETO COM FREIO A DISCO HIDRÁULICO SHIMANO",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/bicicleta-gios-frx-evo-aro-26-amarelo-e-preto-com-freio-a-disco-hidraulico-shimano-671aa48173340.jpg",
+        title:"BICICLETA GIOS FRX-EVO ARO 26 - AMARELO E PRETO COM FREIO A DISCO HIDRÁULICO SHIMANO",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/mini-bomba-elleven-reversivel-eco-65eb2c6e13814.jpg",
+        title:"MINI BOMBA ELLEVEN - REVERSIVEL ECO",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/cadeado-elleven-100cm-15mm-64f0a95f2cf5a.jpg",
+        title:"CADEADO ELLEVEN - 100CM/ 15MM",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/cadeado-elleven-1m-12mm-64f09d81465b4.jpg",
+        title:"CADEADO ELLEVEN - 1M/ 12MM                                          ",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/cadeado-elleven-100cm-18mm-64f0aa69d2bcc.jpg",
+        title:"CADEADO ELLEVEN - 100CM/ 18MM",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/farol-pisca-elleven-power-light-64ee3a49067b0.jpg",
+        title:"FAROL + PISCA ELLEVEN - POWER LIGHT",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/farol-elleven-dianteiro-2-leds-65eb32c02f731.jpg",
+        title:"FAROL ELLEVEN DIANTEIRO 2 LEDS",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/pisca-led-light-superflash-70-lumens-usb-660b09c28c97e.jpg",
+        title:"PISCA LED LIGHT SUPERFLASH 70 LÚMENS USB",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/suporte-de-celular-para-bike-4003.jpg",
+        title:"SUPORTE DE CELULAR PARA BIKE",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/capacete-absolute-nero-vermelho-e-branco-m-642d6230e4bf9.jpg",
+        title:"CAPACETE ABSOLUTE NERO - VERMELHO E BRANCO M",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/capacete-absolute-nero-azul-e-branco-m-642d6208ab7d6.jpg",
+        title:"CAPACETE ABSOLUTE NERO - AZUL E BRANCO M",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/capacete-absolute-nero-preto-e-rosa-m-642d61e20fd3b.jpg",
+        title:"CAPACETE ABSOLUTE NERO - PRETO E ROSA M",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/pedal-plataforma-de-aluminio-polido-66cc817260914.jpg",
+        title:"PEDAL PLATAFORMA DE ALUMÍNIO POLIDO",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/pedal-plataforma-pro-x-roxo-eixo-9-16-662953c43b526.jpg",
+        title:"PEDAL PLATAFORMA PRO-X ROXO EIXO 9/16",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/pedal-plataforma-pro-x-eixo-9-16-66295d69af27a.jpg",
+        title:"PEDAL PLATAFORMA PRO-X EIXO 9/16",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/quadro-gios-4freaks-branco-e-roxo-aro-26-67003d453796f.jpg",
+        title:"QUADRO GIOS 4FREAKS - BRANCO E ROXO (ARO 26)",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/quadro-gios-4freaks-preto-e-roxo-aro-26-67003d8d2b596.jpg",
+        title:"QUADRO GIOS 4FREAKS - PRETO E ROXO (ARO 26)",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/quadro-gios-4freaks-azul-marinho-e-azul-aro-26-67003db984f07.jpg",
+        title:"QUADRO GIOS 4FREAKS - AZUL MARINHO E AZUL (ARO 26)",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/esticador-de-corrente-preto-par-662bb85c93c11.jpg",
+        title:"ESTICADOR DE CORRENTE - PRETO (UN)",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/balaclava-de-ciclismo-castelli-winderstopper-inverno-preta-64ed0196849cc.png",
+        title:"Balaclava de Ciclismo Castelli Winderstopper Inverno PretaCod. do Produto: 490728-490728 ",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/mascara-de-ciclismo-castelli-windstopper-viso-face-preta-64de80af6918a.png",
+        title:"Mascara de Ciclismo Castelli Windstopper Viso Face Preta",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/balaclava-de-ciclismo-curtlo-thermoskin-articulada-preta-6657587e5678a.jpg",
+        title:"Balaclava de Ciclismo Curtlo Thermoskin Articulada Preta",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/jaqueta-de-ciclismo-orbea-hiru-core-thermal-inverno-azul-670fbaed50a41.jpg",
+        title:"Jaqueta de Ciclismo Orbea Hiru Core Thermal Inverno Azul",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/jaqueta-de-ciclismo-corta-vento-ale-elements-preta-66a77f829fcdf.jpg",
+        title:"Jaqueta de Ciclismo Corta Vento ALÉ Elements Preta",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/jaqueta-de-ciclismo-shimano-furano-inverno-verde-66955c11f1340.jpg",
+        title:"Jaqueta de Ciclismo Shimano Furano Inverno Verde",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/macaquinho-de-ciclismo-marcio-may-ultra-black-manga-longa-67053ac2e6b26.jpg",
+        title:"Macaquinho de Ciclismo Marcio May Ultra Black Manga Longa",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/macaquinho-de-ciclismo-ert-new-elite-venom-preto-670d184f6c07b.jpg",
+        title:"Macaquinho de Ciclismo ERT New Elite Venom Preto",
+        description: "comprar",
+    },
+    {
+        Image: "fotos/macaquinho-de-ciclismo-ert-new-elite-gel-team-preto-e-rosa-670d2475c8bbb.jpg",
+        title:"Macaquinho de Ciclismo ERT New Elite Gel Team Preto e Rosa",
+        description: "comprar",
+    },
+   
+   
+]
+const cardContainer = document.querySelector(".card-container");
+const searchInput = document.querySelector("#searchInput");
+
+
+
+
+const displayData = data => {
+    cardContainer.innerHTML = "";
+    data.forEach(e => {
+        cardContainer.innerHTML += `
+        <div class ="column">
+           <img src="${e.Image}" alt="error">
+            <h3>${e.title}</h3>
+            <a href="compras.html"><button>${e.description}</button></a>
+        </div>
+        `
+    });
+}
+
+
+
+
+searchInput.addEventListener("keyup", (e) =>{
+    const search = data.filter(i => i.title.toLocaleLowerCase().includes(e.target.value.toLocaleLowerCase()));
+    displayData(search);
+})
+
+
+
+
+window.addEventListener("load", displayData.bind(null,data))
